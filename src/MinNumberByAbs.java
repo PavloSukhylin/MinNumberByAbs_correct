06f6ce972f845cd506f0534a9ef8a4e5b9e0a143
PavloSukhylin <supana1976@gmail.com>
2021-03-25 22:49:47 +0300

    correct view HW3

---
+++ src/MinNumberByAbs.java
@@ -0,0 +1,13 @@
+public class MinNumberByAbs {
+        public static void main(String[] args) {
+            float firstNumber = -23f;
+            float secondNumber = 108f;
+            float thirdNumber = -90f;
+            float firstAbsNumber = Math.abs(firstNumber);
+            float secondAbsNumber = Math.abs(secondNumber);
+            float thirdAbsNumber = Math.abs(thirdNumber);
+            float minNumber = (Math.min(firstAbsNumber,secondAbsNumber) < thirdAbsNumber ) ? Math.min(firstAbsNumber, secondAbsNumber) : thirdAbsNumber;
+            System.out.println("Наименьшее по модулю число " + minNumber);
+
+        }
+}
